3afff166107572c05f075b8e25aed06fb2553d49
hardlightxda@gmail.com <hardlightxda@gmail.com>
2016-12-29 12:11:05 +0000

    Build gdisk for android
    Change-Id: I0aed25191d41211f8852e5cb1c0a53e19ebe7223

--- Android.mk
+++ Android.mk
@@ -18,6 +18,24 @@ sgdisk_src_files := \
     diskio-unix.cc \
     android_popt.cc \
 
+gdisk_src_files := \
+    gdisk.cc \
+    gpttext.cc \
+    crc32.cc \
+    support.cc \
+    guid.cc \
+    gptpart.cc \
+    mbrpart.cc \
+    basicmbr.cc \
+    mbr.cc \
+    gpt.cc \
+    bsd.cc \
+    parttypes.cc \
+    attributes.cc \
+    diskio.cc \
+    diskio-unix.cc \
+    android_popt.cc \
+
 include $(CLEAR_VARS)
 
 LOCAL_CPP_EXTENSION := .cc
@@ -29,6 +47,20 @@ LOCAL_CFLAGS += -Wno-unused-parameter -Werror
 LOCAL_SHARED_LIBRARIES := libext2_uuid libstlport
 
 LOCAL_MODULE := sgdisk
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_CPP_EXTENSION := .cc
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) external/e2fsprogs/lib external/stlport/stlport bionic
+LOCAL_SRC_FILES := $(gdisk_src_files)
+LOCAL_CFLAGS += -Wno-unused-parameter -Werror
+
+LOCAL_SHARED_LIBRARIES := libext2_uuid libstlport
+
+LOCAL_MODULE := gdisk
 
 include $(BUILD_EXECUTABLE)
 
@@ -48,5 +80,24 @@ LOCAL_SHARED_LIBRARIES := libext2_uuid-host
 
 LOCAL_MODULE := sgdisk_host
 LOCAL_MODULE_STEM := sgdisk
+
+include $(BUILD_HOST_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_CPP_EXTENSION := .cc
+
+ifeq ($(HOST_OS),darwin)
+LOCAL_CFLAGS := -D_FILE_OFFSET_BITS=64 -Doff64_t=off_t
+endif
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) external/e2fsprogs/lib
+LOCAL_SRC_FILES := $(gdisk_src_files)
+LOCAL_CFLAGS += -Wno-unused-parameter -Werror
+
+LOCAL_SHARED_LIBRARIES := libext2_uuid-host
+
+LOCAL_MODULE := gdisk_host
+LOCAL_MODULE_STEM := gdisk
 
 include $(BUILD_HOST_EXECUTABLE)
